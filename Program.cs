2e178378859a36f9f7541e65eddbb06f61643fc1
Adam <typeof.adam@gmail.com>
2020-05-30 18:56:07 +0100

    Some VScode stuff

--- Program.cs
+++ Program.cs
@@ -7,23 +7,8 @@ namespace Blackjack
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            Card card;
-
             deck.Shuffle();
             
-            card = deck.Deal();
-            Console.WriteLine($"{card.Rank}");
-            card = deck.Deal();
-            Console.WriteLine($"{card.Rank}");
-            card = deck.Deal();
-            Console.WriteLine($"{card.Rank}");
-            card = deck.Deal();
-            Console.WriteLine($"{card.Rank}");
-            card = deck.Deal();
-            Console.WriteLine($"{card.Rank}");
-            card = deck.Deal();
-            Console.WriteLine($"{card.Rank}");
-
         }
     }
 }
