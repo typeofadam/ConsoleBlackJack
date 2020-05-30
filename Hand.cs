6c28e88985565b501236f341af6213988f0b880b
Adam <typeof.adam@gmail.com>
2020-05-30 19:28:06 +0100

    Logic for adding cards from a deck to hand tested. Along with score calculation

--- Hand.cs
+++ Hand.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 namespace Blackjack {
     public class Hand {
         private List<Card> cards;
-        public int score {get; private set;}
+        public int Score {get; private set;}
         
         public Hand() {
             cards = new List<Card>();
-            score = 0;
+            Score = 0;
         }
 
         public void AddCard(Card card) {
@@ -28,7 +28,7 @@ namespace Blackjack {
                 if(card.Rank == 'A') hasAce = true;
                 if(hasAce && value <= 11) value += 10;
             }
-            score = value;
+            Score = value;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ namespace Blackjack
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            Hand hand = new Hand();
             deck.Shuffle();
+
+            hand.AddCard(deck.Deal());
+            hand.AddCard(deck.Deal());
+
+            var cards = hand.GetHand();
+            foreach(Card card in cards) {
+                Console.WriteLine(card.Rank);
+            }
+            Console.WriteLine(hand.Score);
+            Console.WriteLine(hand.Score);
             
         }
     }
